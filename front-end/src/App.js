219922efc42709be93176294b90324c654bc1dd4
Alex Mwaura <alexmwaura36@gmail.com>
2022-09-11 13:48:57 +0300

    used npm install command to install all dependencis for the react app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import './App.css';
-// import {  Route, Routes } from 'react-router-dom';
-// import Comedy from './components/Comedy';
-// import Drama from './components/Drama';
-// import Home from './components/Home';
-// import Kids from './components/Kids';
-// import Trending from './components/Trending';
-// import NavBar from './components/NavBar';
-// import Reviews from './components/Reviews';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavBar />
-//       <Routes>
-//           <Route exact path="/" element={<Home />}></Route>
-//           <Route path="/trending" element={<Trending/>}></Route>
-//           <Route path="/kids" element={<Kids/>}></Route>
-//           <Route path="/drama" element={<Drama/>}></Route>
-//           <Route path="/comedy" element={<Comedy/>}></Route>
-//           <Route path="/reviews" element={<Reviews/>}></Route>
-//       </Routes>
-//     </div>
-//   );
-// }
+import React from 'react';
+import './App.css';
+import {  Route, Routes } from 'react-router-dom';
+import Comedy from './components/Comedy';
+import Drama from './components/Drama';
+import Home from './components/Home';
+import Kids from './components/Kids';
+import Trending from './components/Trending';
+import NavBar from './components/NavBar';
+import Reviews from './components/Reviews';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/trending" element={<Trending/>}></Route>
+          <Route path="/kids" element={<Kids/>}></Route>
+          <Route path="/drama" element={<Drama/>}></Route>
+          <Route path="/comedy" element={<Comedy/>}></Route>
+          <Route path="/reviews" element={<Reviews/>}></Route>
+      </Routes>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
