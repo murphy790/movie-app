acaf5217e04afd357188c319c6b123a2749d321d
Alex Mwaura <alexmwaura36@gmail.com>
2022-09-11 14:01:16 +0300

    added the category column

--- back-end/db/migrate/20220907162006_create_movies.rb
+++ back-end/db/migrate/20220907162006_create_movies.rb
@@ -1,10 +1,10 @@
-# class CreateMovies < ActiveRecord::Migration[6.1]
-#   def change
-#     create_table :movies do |t|
-#       t.string :title
-#       t.string :overview
-#       t.string :release_date
-#       t.integer :vote_average
-#     end
-#   end
-# end
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :overview
+      t.string :release_date
+      t.integer :vote_average
+    end
+  end
+end
