75f6e528c1e99917a9a66e0d87a2e18ff5acd2dd
Alex Mwaura <alexmwaura36@gmail.com>
2022-09-11 13:51:44 +0300

    installed needed dependencies by running bundle install

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -1,42 +1,42 @@
-class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
+# class ApplicationController < Sinatra::Base
+#   set :default_content_type, 'application/json'
   
-  get '/movies' do
-    movies = Movie.all.order(:title).limit(10)
-    movies.to_json
-  end
+#   get '/movies' do
+#     movies = Movie.all.order(:title).limit(10)
+#     movies.to_json
+#   end
 
-  get '/reviews' do
-    reviews = Review.all
-    reviews.to_json
-  end
+#   get '/reviews' do
+#     reviews = Review.all
+#     reviews.to_json
+#   end
   
-  get '/movies/:category' do
-    movie = Movie.find_by( category:params[:category])
-    movie.to_json
-  end
-  post '/reviews/:id' do
-    review = Review.create(
-      score: params[:score],
-      comment: params[:comment],
-      movie_id: params[:movie_id],
-      user_id: params[:user_id]
-    )
-    review.to_json
-  end
+#   get '/movies/:category' do
+#     movie = Movie.find_by( category:params[:category])
+#     movie.to_json
+#   end
+#   post '/reviews/:id' do
+#     review = Review.create(
+#       score: params[:score],
+#       comment: params[:comment],
+#       movie_id: params[:movie_id],
+#       user_id: params[:user_id]
+#     )
+#     review.to_json
+#   end
 
-  delete '/reviews/:id' do
-    review = Review.find(params[:id])
-    review.destroy
-    review.to_json
-  end
+#   delete '/reviews/:id' do
+#     review = Review.find(params[:id])
+#     review.destroy
+#     review.to_json
+#   end
 
-  patch '/reviews/:id' do
-    review = Review.find(params[:id])
-    review.update(
-      score: params[:score],
-      comment: params[:comment]
-    )
-    review.to_json
-  end
-end
+#   patch '/reviews/:id' do
+#     review = Review.find(params[:id])
+#     review.update(
+#       score: params[:score],
+#       comment: params[:comment]
+#     )
+#     review.to_json
+#   end
+# end
